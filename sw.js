fc71e43cfbd6c4d368735e9146f17ad2d4d274e7
resulmilaimi91-web <resulmilaimi91@gmail.com>
2026-06-01 23:15:18 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'fittracker-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/exercises.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
